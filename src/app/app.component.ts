4e897bb1cc0b2cd9d7146032c843b1ce6abd5bd1
Andrew Caldas <andrewaguiar@ibm.com>
2021-03-01 14:48:23 -0300

    side y nav bar templates, routes y 2 views created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+
+import jQuery from 'jquery';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'app-clientes';
+
+  ngAfterViewInit(): void {
+    ($ => {
+      'use strict';
+
+      // Add active state to sidbar nav links
+      const path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+      // tslint:disable-next-line:typedef
+      $('#layoutSidenav_nav .sb-sidenav a.nav-link').each(function() {
+        if (this.href === path) {
+          $(this).addClass('active');
+        }
+      });
+
+      // Toggle the side navigation
+      // tslint:disable-next-line:only-arrow-functions
+      $('#sidebarToggle').on('click', function(e: any): void {
+        e.preventDefault();
+        $('body').toggleClass('sb-sidenav-toggled');
+      });
+    })(jQuery);
+  }
 }
